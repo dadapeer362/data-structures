742b2fef4352db7c9264001e32423eb3a9a3da4c
Shaik Dadapeer <shaik.dadapeer@financepeer.co>
2023-10-10 08:44:14 +0530

    linked list methods

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -40,10 +40,117 @@ class LinkedList:
             self.head = new_node
         self.length += 1
 
+    def insert(self, index, value):
+        new_node = Node(value)
+        if index < 0 and index > self.length:
+            return False
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+            for _ in range(index - 1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length += 1
+        return True
+    
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
+    def search(self, target):
+        current = self.head
+        index = 0
+        while current:
+            if current.value == target:
+                return index
+            current = current.next
+            index += 1
+        return -1
+    
+    def get(self, index):
+        if index == -1:
+            return self.tail
+        if index < 0 and index > self.length:
+            return -1
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        popped_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        popped_node = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp.next is not self.tail:
+                temp = temp.next
+            self.tail = temp
+            temp.next = None
+        self.length -= 1
+        return popped_node
+    
+    def remove(self, index):
+        if index >= self.length or index < -1:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1 or index == -1:
+            return self.pop()
+        pre_node = self.get(index - 1)
+        popped_node = pre_node.next
+        pre_node.next = popped_node.next
+        popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+
         
 new_linked_list = LinkedList()
 new_linked_list.append(36)
 new_linked_list.append(77)
 new_linked_list.append(33)
 new_linked_list.prepend(44)
+new_linked_list.insert(3, 66)
+new_linked_list.traverse()
+print(new_linked_list.search(36))
+print(new_linked_list.get(2))
+print(new_linked_list.set_value(2, 40))
+print(new_linked_list.pop_first())
 print(new_linked_list)
